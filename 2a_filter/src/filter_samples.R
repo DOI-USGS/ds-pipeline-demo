b4d351b2b7afe77d05f557de0ca9a1668cacadf6
Laura DeCicco <ldecicco@usgs.gov>
2017-08-16 10:41:20 -0500

    Making eLists with progress

--- 2a_filter/src/filter_samples.R
+++ 2a_filter/src/filter_samples.R
@@ -7,31 +7,22 @@ filter_samples <- function(filter.config){
   config.args <- yaml.load_file(filter.config)
   
   fetch.args <- config.args$fetch.args
-  file.data <- fetch.args[["file.data"]]
-  fetch.path <- fetch.args[["path"]]
-  
-  site.data <- fetch.args[["site.data"]]
-  site.path <- fetch.args[["site.path"]]
-  site.sheet <- fetch.args[["sheet"]]
-  
   save.args <- config.args$save.args
-  save.path <- save.args[["save.path"]]
-  save.file <- save.args[["save.file"]]
-  
+
   filter.count <- config.args$filter.args
   
-  data.wide <- readRDS(file.path(fetch.path,file.data))
+  data.wide <- readRDS(file.path(fetch.args[["path"]],fetch.args[["file.data"]]))
   
   summary.sites <- data.wide %>%
     group_by(SITE) %>%
     summarize(begin = min(DATE,na.rm = TRUE),
               end = max(DATE,na.rm = TRUE),
-              count = n())
-  
-  sheet.names <- excel_sheets(file.path(site.path,site.data))
-  
-  site.info <- read_excel(file.path(site.path,site.data), 
-                          sheet = site.sheet)
+              count = n()) %>%
+    filter(count > filter.count[["min.samples"]]) %>%
+    arrange(desc(count)) 
+ 
+  site.info <- read_excel(file.path(fetch.args[["site.path"]],fetch.args[["site.data"]]), 
+                          sheet = fetch.args[["sheet"]])
   
   flow.info <- select(site.info, Site, `USGS Flow Site to use`)
   
@@ -42,21 +33,20 @@ filter_samples <- function(filter.config){
   summary.sites$depth_code <- gsub(pattern = "[A-Za-z]{2}-[0-9]{2}",replacement = "", summary.sites$SITE)
   
   summary.sites <- summary.sites %>%
-    left_join(flow.info, by=c("main_site"="Site"))
+    left_join(flow.info, by=c("main_site"="Site")) %>%
+    rename(siteID = `USGS Flow Site to use`) %>%
+    filter(!is.na(siteID)) 
   
   summary.flow <- summary.sites %>%
-    rename(siteID = `USGS Flow Site to use`) %>%
-    filter(count > filter.count[["min.samples"]]) %>%
-    filter(!is.na(siteID)) %>%
-    arrange(desc(count)) %>%
     group_by(siteID) %>%
     summarise(start = min(begin),
               end = max(end))
   
   
-  dir.create(file.path(save.path),recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(save.args[["save.path"]]),recursive = TRUE, showWarnings = FALSE)
   
-  saveRDS(summary.flow, file=file.path(save.path,save.file))
+  saveRDS(summary.flow, file=file.path(save.args[["save.path"]],save.args[["save.flow"]]))
+  saveRDS(summary.sites, file=file.path(save.args[["save.path"]],save.args[["save.site"]]))
     
 }
 
--- 3_merge/src/merge_sample_flow.R
+++ 3_merge/src/merge_sample_flow.R
@@ -1,26 +1,109 @@
 library(EGRET)
 library(dplyr)
+library(yaml)
 
 merge_sample_flow <- function(merge_config){
   
   config.args <- yaml.load_file(merge_config)
   
   fetch.args <- config.args$fetch.args
-  flow.path <- fetch.args[["flow.path"]]
-  flow.file <- fetch.args[["flow.file"]]
-  sample.path <- fetch.args[["sample.path"]]
-  sample.file <- fetch.args[["sample.file"]]
-  site.path <- fetch.args[["site.path"]]
-  site.file <- fetch.args[["site.file"]]
-  
-  all.samples <- readRDS(file.path(sample.path,sample.file))
-  all.flow <- readRDS(file.path(flow.path,flow.file))
-  
-  site.summary <- readRDS(file.path(site.path,site.file))
-  
   save.args <- config.args$save.args
-  save.path <- save.args[["save.path"]]
-  save.file <- save.args[["save.file"]]
+  params <- data.frame(config.args$params, stringsAsFactors = FALSE)
+
+  all.samples <- readRDS(file.path(fetch.args[["sample.path"]],fetch.args[["sample.file"]]))
+  all.flow <- readRDS(file.path(fetch.args[["flow.path"]],fetch.args[["flow.file"]]))
+  site.summary <- readRDS(file.path(fetch.args[["site.path"]],fetch.args[["site.file"]]))
+  
+
+  dir.create(file.path(save.args[["save.path"]]),recursive = TRUE, showWarnings = FALSE)
+  pdf(file = file.path(save.args[["save.path"]],save.args[["save.graph"]]))
+  
+  master_list <- data.frame(id = character(),
+                            complete = logical(),
+                            missing_all_sample = logical(),
+                            missing_all_flow = logical(),
+                            stringsAsFactors = FALSE)
+  
+  
+  for(i in site.summary$SITE){
+    sample.data <- filter(all.samples, SITE == i)
+    flow_site <- site.summary$siteID[which(site.summary$SITE == i)]
+    flow <- filter(all.flow, site_no == flow_site)
+    if(nrow(flow) == 0){
+      master_list <- bind_rows(master_list, 
+                               data.frame(id = paste(i, params$paramShortName, sep="_"),
+                                          complete = FALSE,
+                                          missing_all_sample = FALSE,
+                                          missing_all_flow = TRUE,
+                                          stringsAsFactors = FALSE))
+      saveRDS(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]))
+      next
+    }
+    names(flow) <- c('agency', 'site', 'dateTime', 'value', 'code')
+    
+    Daily <- populateDaily(flow, 35.314667,verbose = FALSE)
+    
+    for(j in seq_len(nrow(params))){
+      
+      sample.sub <- sample.data[,c("DATE",params$name[j],
+                                   paste("rmk",params$name[j],sep = "_"))]
+      names(sample.sub) <- c("dateTime", "value", "code")
+      sample.sub <- sample.sub[,c("dateTime", "code","value")]
+      sample.sub <- sample.sub[!is.na(sample.sub$value),]
+      if(nrow(sample.sub) == 0){
+        master_list <- bind_rows(master_list, 
+                                 data.frame(id = paste(i, params$paramShortName[j], sep="_"),
+                                            complete = FALSE,
+                                            missing_all_sample = TRUE,
+                                            missing_all_flow = FALSE,
+                                            stringsAsFactors = FALSE))
+        saveRDS(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]))
+        next
+      }
+      compressedData <- compressData(sample.sub, verbose=FALSE)
+      Sample <- populateSampleColumns(compressedData)
+      INFO <- data.frame(paramShortName = params$paramShortName[j],
+                         param.units = params$param.units[j],
+                         shortName = i,
+                         constitAbbrev = paste(params$paramShortName[j],i,sep="_"),
+                         staAbbrev = i,
+                         paStart = 10,
+                         paLong = 12,
+                         stringsAsFactors = FALSE)
+      
+      Sample <- filter(Sample, Date %in% Daily$Date)
+      
+      if(nrow(Sample) < config.args$checks[["min.samples"]]){
+        master_list <- bind_rows(master_list, 
+                                 data.frame(id = paste(i, params$paramShortName[j], sep="_"),
+                                            complete = FALSE,
+                                            missing_all_sample = TRUE,
+                                            missing_all_flow = FALSE,
+                                            stringsAsFactors = FALSE))
+        saveRDS(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]))
+        next
+      }
+      
+      eList <- mergeReport(INFO,Daily,Sample,verbose = FALSE)
+      saveRDS(eList, file = file.path(save.args[["save.path"]],paste0(i,"_",params$paramShortName[j],".rds")))
+      
+      plot(eList)
+      
+      master_list <- bind_rows(master_list, 
+                               data.frame(id = paste(i, params$paramShortName[j], sep="_"),
+                                          complete = TRUE,
+                                          missing_all_sample = FALSE,
+                                          missing_all_flow = FALSE,
+                                          stringsAsFactors = FALSE))
+      saveRDS(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]))
+    }
+    
+  }
+  
+  dev.off()
+  
+  
+
 }
 
 merge_sample_flow(merge_config = "3_merge/in/merge_config.yaml")
