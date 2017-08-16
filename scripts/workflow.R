62c5bd14693326f9d48d46472eac5be0e776e8b7
Laura DeCicco <ldecicco@usgs.gov>
2017-08-16 11:01:42 -0500

    cleanup old workflow

--- scripts/workflow.R
+++
@@ -1,6 +0,0 @@
-# overall workflow:
-source("scripts/download/download_from_AWS.R")
-source("scripts/process/process_sample_data.R")
-source("scripts/process/get_flow.R")
-source("scripts/process/make_eList.R")
-source("scripts/model/run_models.R")
