a798f258e2a3a848a1c873737a54edef387a2887
Alison Appling <aappling@usgs.gov>
2017-08-21 17:16:12 -0700

    add "helper file" to try out multiple sources per step

---
+++ 1_get_raw_data/src/help-get_siteinfo.R
@@ -0,0 +1,4 @@
+# Example file to demonstrate that multiple dependencies will be loaded by the makefile macro
+message('sourced help-get_siteinfo.R')
+
+# gratuitous helper file to test making/sourcing multiple scripts
