4d907f71f46a8b0af614137e03b175adf92eaed5
Laura DeCicco <ldecicco@usgs.gov>
2017-08-23 16:35:16 -0500

    Better use of doc and log folder

--- 5_merge/src/merge_sample_flow.R
+++ 5_merge/src/merge_sample_flow.R
@@ -8,6 +8,9 @@ merge_sample_flow <- function(merge_config){
   
   fetch.args <- config.args$fetch.args
   save.args <- config.args$save.args
+  explore.args <- config.args$explore.args
+  log.args <- config.args$log.args
+  
   params <- data.frame(config.args$params, stringsAsFactors = FALSE)
 
   all.samples <- readRDS(file.path(fetch.args[["sample.path"]],fetch.args[["sample.file"]]))
@@ -15,8 +18,11 @@ merge_sample_flow <- function(merge_config){
   site.summary <- readRDS(file.path(fetch.args[["site.path"]],fetch.args[["site.file"]]))
   
 
-  dir.create(file.path(save.args[["save.path"]]),recursive = TRUE, showWarnings = FALSE)
-  pdf(file = file.path(save.args[["save.path"]],save.args[["save.graph"]]))
+  dir.create(file.path(log.args[["path"]]),recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(explore.args[["path"]]),recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(save.args[["path"]]),recursive = TRUE, showWarnings = FALSE)
+  
+  pdf(file = file.path(explore.args[["path"]],explore.args[["graph"]]))
   
   master_list <- data.frame(id = character(),
                             complete = logical(),
@@ -36,7 +42,7 @@ merge_sample_flow <- function(merge_config){
                                           missing_all_sample = FALSE,
                                           missing_all_flow = TRUE,
                                           stringsAsFactors = FALSE))
-      write.csv(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]),row.names = FALSE)
+      write.csv(master_list, file = file.path(log.args[["path"]],log.args[["file"]]),row.names = FALSE)
       next
     }
     names(flow) <- c('agency', 'site', 'dateTime', 'value', 'code')
@@ -45,6 +51,8 @@ merge_sample_flow <- function(merge_config){
     
     for(j in seq_len(nrow(params))){
       
+      id <- paste0(i,"_",params$paramShortName[j])
+      
       sample.sub <- sample.data[,c("DATE",params$name[j],
                                    paste("rmk",params$name[j],sep = "_"))]
       names(sample.sub) <- c("dateTime", "value", "code")
@@ -52,12 +60,12 @@ merge_sample_flow <- function(merge_config){
       sample.sub <- sample.sub[!is.na(sample.sub$value),]
       if(nrow(sample.sub) == 0){
         master_list <- bind_rows(master_list, 
-                                 data.frame(id = paste(i, params$paramShortName[j], sep="_"),
+                                 data.frame(id = id,
                                             complete = FALSE,
                                             missing_all_sample = TRUE,
                                             missing_all_flow = FALSE,
                                             stringsAsFactors = FALSE))
-        write.csv(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]),row.names = FALSE)
+        write.csv(master_list, file = file.path(log.args[["path"]],log.args[["file"]]),row.names = FALSE)
         next
       }
       compressedData <- compressData(sample.sub, verbose=FALSE)
@@ -65,7 +73,7 @@ merge_sample_flow <- function(merge_config){
       INFO <- data.frame(paramShortName = params$paramShortName[j],
                          param.units = params$param.units[j],
                          shortName = i,
-                         constitAbbrev = paste(params$paramShortName[j],i,sep="_"),
+                         constitAbbrev = id,
                          staAbbrev = i,
                          paStart = 10,
                          paLong = 12,
@@ -75,27 +83,28 @@ merge_sample_flow <- function(merge_config){
       
       if(nrow(Sample) < config.args$checks[["min.samples"]]){
         master_list <- bind_rows(master_list, 
-                                 data.frame(id = paste(i, params$paramShortName[j], sep="_"),
+                                 data.frame(id = id,
                                             complete = FALSE,
                                             missing_all_sample = TRUE,
                                             missing_all_flow = FALSE,
                                             stringsAsFactors = FALSE))
-        write.csv(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]),row.names = FALSE)
+        write.csv(master_list, file = file.path(log.args[["path"]],log.args[["file"]]),row.names = FALSE)
         next
       }
       
       eList <- mergeReport(INFO,Daily,Sample,verbose = FALSE)
-      saveRDS(eList, file = file.path(save.args[["save.path"]],paste0(i,"_",params$paramShortName[j],".rds")))
+      
+      saveRDS(eList, file = file.path(save.args[["path"]],paste0(id,".rds")))
       
       plot(eList)
       
       master_list <- bind_rows(master_list, 
-                               data.frame(id = paste(i, params$paramShortName[j], sep="_"),
+                               data.frame(id = id,
                                           complete = TRUE,
                                           missing_all_sample = FALSE,
                                           missing_all_flow = FALSE,
                                           stringsAsFactors = FALSE))
-      write.csv(master_list, file = file.path(save.args[["save.path"]],save.args[["save.file"]]),row.names = FALSE)
+      write.csv(master_list, file = file.path(log.args[["path"]],log.args[["file"]]),row.names = FALSE)
     }
     
   }
