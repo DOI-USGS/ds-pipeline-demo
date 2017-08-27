e3d6ead63c7009a076b4b2624c1d24463769b83c
Alison Appling <aappling@usgs.gov>
2017-08-27 12:19:58 -0700

    make post_s3 conditional on a switch in s3_post.yaml

--- build/cache.mak
+++ build/cache.mak
@@ -21,15 +21,15 @@ lib/out/s3_inventory.tsv :\
 # Macro for attaching to the end of an RSCRIPT expression. posts the data file to S3, creates an .s3 status indictor file, and updates the data file's timestamp so make understands that it's up to date relative to the status file. We could switch the updating from bytrust to fromcache, at the cost of making more status check calls to S3.
 POSTS3=-e 'post_s3(file.name="$(subst .s3,,$@)", s3.config="lib/cfg/s3_config.yaml")'\
 		${ADDLOG};\
-	${DATETIME} > $@;\
 	touch $(subst .s3,,$@)
 
 # Generic rule for downloading a data file from S3, which will only be attempted if the corresponding status file already exists. A data file is out of date if it's older than its .s3 indicator file. With a shared-cache project, 'making' a data file actually means either (1) pulling the data file from the cache or (2) making the indicator file, where (2) always has a byproduct of recomputing the data file and posting it to the cache. If we call `make datafile` and #2 applies, then it's worth a timestamp check after the .s3 dependency is built and before trying the download, because the data fila may already exist locally as a side effect of building the .s3 indicator file. `make` won't have noticed for us, so we need to check ourselves.
 # [ x -ot y ] tests whether x is older than y to within ~1 second
 # [... -o ! -e x ] adds an OR test for whether x is a missing file
 # the sequence from if to fi needs to be a single make line, hence the \'s at the ends of lines
+# prerequisites following the | separator are 'order-only': their presence is required but their timestamp is ignored, so the target isn't rebuilt if these prerequisites are updated
 % : %.s3\
-		lib/src/s3.R lib/cfg/s3_config.yaml
+		lib/src/s3.R lib/cfg/s3_config.yaml | lib/cfg/s3_post.yaml
 	@if [ $@ -ot $@.s3 -o ! -e $@ ]; then\
 		${RSCRIPT}\
 		-e 'get_s3(file.name="$@", s3.config="lib/cfg/s3_config.yaml")'\
--- lib/src/s3.R
+++ lib/src/s3.R
@@ -6,7 +6,7 @@ library(aws.s3)
 library(yaml)
 library(dplyr)
 
-get_s3 <- function(file.name, s3.config="lib/s3_config.yaml") {
+get_s3 <- function(file.name, s3.config="lib/cfg/s3_config.yaml") {
   
   message("Downloading ", file.name, " from S3")
   s3_config <- yaml.load_file(s3.config)
@@ -17,35 +17,47 @@ get_s3 <- function(file.name, s3.config="lib/s3_config.yaml") {
   
 }
 
-post_s3 <- function(file.name, s3.config="lib/s3_config.yaml") {
+post_s3 <- function(file.name, s3.config="lib/cfg/s3_config.yaml", s3.post="lib/cfg/s3_post.yaml") {
   
-  message("Uploading ", file.name, " to S3")
-  s3_config <- yaml.load_file(s3.config)
-  use_credentials(profile = s3_config$s3Profile)
-  put_object(file = file.name,
-             object = file.path(s3_config$s3Path, basename(file.name)), 
-             bucket = s3_config$bucket)
+  s3_post <- yaml.load_file(s3.post)
+  if(s3_post$post) {
+    # download the file from S3 to the local file.name
+    s3_config <- yaml.load_file(s3.config)
+    message("Uploading ", file.name, " to S3 because s3_post.yaml says 'post: TRUE'")
+    use_credentials(profile = s3_config$s3Profile)
+    put_object(file = file.name,
+               object = file.path(s3_config$s3Path, basename(file.name)), 
+               bucket = s3_config$bucket)
+    
+    # write the indicator file (but only if(s3_post))
+    timestamp <- format(st, tz="UTC", format='%Y-%m-%d %H:%M:%S 0000')
+    writeLines(timestamp, con=paste0(file.name, '.s3'))
+    
+  } else {
+    message("Skipping upload of ", file.name, " to S3 because s3_post.yaml says 'post: FALSE'")
+  }
   
 }
 
-list_s3 <- function(s3.config="lib/s3_config.yaml") {
+list_s3 <- function(s3.config="lib/cfg/s3_config.yaml", prefix="s3Path") {
   
   message("Listing project files on S3")
   s3_config <- yaml.load_file(s3.config)
+  if(prefix=="s3Path") prefix <- s3_config$s3Path
   use_credentials(profile = s3_config$s3Profile)
-  bucket_df <- aws.s3::get_bucket_df(bucket=s3_config$bucket)
+  bucket_df <- aws.s3::get_bucket_df(bucket=s3_config$bucket, prefix=prefix)
   dplyr::filter(bucket_df, grepl(sprintf("^%s/.+", s3_config$s3Path),Key))
   
 }
 
-inventory_s3 <- function(s3.inventory="lib/s3_inventory.tsv", s3.config="lib/s3_config.yaml") {
+inventory_s3 <- function(s3.inventory="lib/out/s3_inventory.tsv", s3.config="lib/cfg/s3_config.yaml") {
   
   cache_df <- list_s3()
   write.table(cache_df, file=s3.inventory, sep='\t', row.names=FALSE)
   
 }
 
-make_s3_indicator <- function(file.name, s3.inventory="lib/s3_inventory.tsv", s3.config="lib/s3_config.yaml") {
+make_s3_indicator <- function(file.name, s3.inventory="lib/out/s3_inventory.tsv", s3.config="lib/cfg/s3_config.yaml") {
   
   cache_df <- read.table(s3.inventory, header=TRUE, sep='\t', stringsAsFactors=FALSE, colClasses='character')
   s3_config <- yaml.load_file(s3.config)
